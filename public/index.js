d564cd8ebcfbf7ac7326a1b04426051e7a23fa2b
TheLegendary1248 <1248alex@gmail.com>
2022-09-11 16:54:21 -0400

    Finished?

--- public/index.js
+++ public/index.js
@@ -1,9 +1,73 @@
+const key = "9a200c1e4e59492d8be2a4ae6f7ce19f"
+let {GME, MSFT, DIS, BNTX} = mockData/*fetch(`https://api.twelvedata.com/stocks?symbol=GME,MSFT,DIS,BNTX&apikey=${key}`)*/
+const stocks = [GME, MSFT, DIS, BNTX]
+function getColor(stock){
+    if(stock === "GME"){
+        return 'rgba(61, 161, 61, 0.7)'
+    }
+    if(stock === "MSFT"){
+        return 'rgba(209, 4, 25, 0.7)'
+    }
+    if(stock === "DIS"){
+        return 'rgba(18, 4, 209, 0.7)'
+    }
+    if(stock === "BNTX"){
+        return 'rgba(166, 43, 158, 0.7)'
+    }
+}
+
 async function main() {
 
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
-
+    const ctx = document.getElementById('time-chart').getContext('2d');
+    const myChart = new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: stocks[0].values.reverse().map(value => value.datetime),
+            datasets: stocks.map( stock => ({
+                label: stock.meta.symbol,
+                data: stock.values.map(value => parseFloat(value.high)),
+                backgroundColor:  getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol),
+            }))
+        }
+    });
+    // High Chart
+    new Chart(highestPriceChartCanvas.getContext('2d'), {
+        type: 'bar',
+        data: {
+            labels: stocks.map(stock => stock.meta.symbol),
+            datasets: [{
+                label: 'Highest',
+                backgroundColor: stocks.map(stock => (
+                    getColor(stock.meta.symbol)
+                )),
+                borderColor: stocks.map(stock => (
+                    getColor(stock.meta.symbol)
+                )),
+                data: stocks.map(stock => (
+                    findHighest(stock.values)
+                ))
+            }]
+        }
+    });
+    
+}
+//Oh no, how dare i rip off the solution branch because i cannot
+//be bloody bothered with another JS problem with scarce google results.
+//Today's special was "import declarations may only appear at top level of a module"
+//That would be because I tried importing it(even though you don't need to) due
+//to it not being defined in the slightest. 
+function findHighest(values) {
+    let highest = 0;
+    values.forEach(value => {
+        if (parseFloat(value.high) > highest) {
+            highest = value.high
+        }
+    })
+    return highest
 }
 
 main()
